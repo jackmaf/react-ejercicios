3c41691ccdd7adc8ebd86bc6a746b295dcb64517
jackson florez jimenez <jackmaf0@gmail.com>
2022-03-09 17:03:56 -0500

    segunda forma de utilizar condicion en jsx

--- 2. Ejercicios React Basicos/4/src/components/order_list/index.js
+++ 2. Ejercicios React Basicos/4/src/components/order_list/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import InternalUl from './internal_ul';
+//import InternalUl from './internal_ul';
 function OrderList(props) {
   return (
     <ol>
       {props.array_aux.map(function(data, index_out){
         return (
           <li key={index_out}>
-            <InternalUl childrens={data.childrens} name={data.name}/>
+            {/* <InternalUl childrens={data.childrens} name={data.name}/> */}
+            { InternalUl(data.childrens, data.name) }
           </li>
         );
       })}
     </ol>
   );
+}
+
+function InternalUl(childrens, name){
+  if(childrens.length > 0){
+    return (
+      <React.Fragment>
+        {name}
+        <ul>
+          {childrens.map(function(children, index_in){  
+            return <li key={index_in}>{children}</li>    
+          })}
+        </ul>
+      </React.Fragment>
+    );
+  }else{
+    return(
+      <section>
+        {name}
+      </section>
+    );
+  }
+
 }
 
 export default OrderList;
